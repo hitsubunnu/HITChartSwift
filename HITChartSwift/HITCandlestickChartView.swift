a8f5a0c40d891c377f0750e2c78b4d35419dc46a
Bunu Hitsu <idhitsu@gmail.com>
2018-05-22 17:18:40 +0900

    Add candlestick chart

---
+++ HITChartSwift/HITCandlestickChartView.swift
@@ -0,0 +1,362 @@
+//
+//  HITCandlestickChartView
+//  HITChartSwift
+//
+//  Created by hitsubunnu on 2018/03/22.
+//  Copyright © 2018年 hitsubunnu. All rights reserved.
+//
+
+import UIKit
+
+open class HITCandlestickChartView: UIView {
+    @IBInspectable var background: UIColor = UIColor.white
+    @IBInspectable var upColor: UIColor = UIColor.color("0x639a08")
+    @IBInspectable var downColor: UIColor = UIColor.color("0xe60170")
+    @IBInspectable var titleColor: UIColor = UIColor.color("0x0099aa")
+    @IBInspectable var indicatorColor: UIColor = UIColor.color("0x0099aa")
+    @IBInspectable var backgroundLineColor: UIColor = UIColor.color("0xe6e6e6")
+    @IBInspectable var textColor: UIColor = UIColor.color("0x333333")
+    @IBInspectable var hapticFeedbackEnable: Bool = true
+
+    
+    private let kCandlestickWidth: CGFloat = 10.0
+    private let kWidthRatio: CGFloat = 0.8
+    private let kHeightRatio: CGFloat = 5/9
+    private let kStrokeEndAnimetionKeyPath = "strokeEnd"
+    private let kOpacityAnimetionKeyPath = "opacity"
+    private let kPositionAnimetionKeyPath = "position"
+    private let kSettleDateLimit = 5
+    private let kStartMargin: CGFloat = 80.0
+    private let kHeightMargin: CGFloat = 4.0
+    private let kIconRadius: CGFloat = 3.0
+    private let kIndicatorTextWidth: CGFloat = 90.0
+    private let kIndicatorTextHeight: CGFloat = 16.0
+    
+    private var absMaxPercentage = 0
+    private var max: Double = 0
+    private var min: Double = 0
+    private var values = [(close: Double, open: Double, high: Double, low: Double)]()
+    private var dates = [Date]()
+    private var titles = [String]()
+    private var previousIndex = 0
+    private var cgPoints = [CGPoint]()
+    
+    private let titleText = CATextLayer()
+    private let indicator = CAShapeLayer()
+    private let indicatorIcon = CAShapeLayer()
+    private let indicatorText = CATextLayer()
+    
+    public func draw(_ absMax: Int,
+                     values: [(close: Double, open: Double, high: Double, low: Double)],
+                     label: (max: String, center: String, min: String),
+                     dates: [Date],
+                     titles: [String]) {
+        
+        self.layer.sublayers?.removeAll()
+        self.absMaxPercentage = absMax
+        self.max = values.map{ $0.high }.max() ?? 0.0
+        self.min = values.map{ $0.low }.min() ?? 0.0
+        self.values = values
+        self.dates = dates
+        self.titles = titles
+        self.previousIndex = values.count - 1
+        
+        setupBackground(max: label.max, center: label.center, min: label.min)
+        setupTitle()
+        drawChart(values: values)
+        setupIndicator()
+        setupIndicatorIcon()
+        setupIndicatorText()
+        setupGestureRecognizer()
+    }
+    
+    private func setupBackground(max: String, center: String, min: String) {
+        backgroundColor = background
+        
+        let width = bounds.width
+        let height = bounds.height
+        let x = (width - width * kWidthRatio) / 2
+        let lineWidth = width * kWidthRatio
+        
+        // max
+        let yMax = (height - height * kHeightRatio) / 2
+        setupBackgroundLine(x: x, y: yMax, width: lineWidth)
+        setupBackgroundText(x: x, y: yMax, string: max)
+        
+        // center
+        let yCenter = height / 2
+        setupBackgroundCenterLine(x: x, y: yCenter, width: lineWidth)
+        setupBackgroundText(x: x, y: yCenter, string: center)
+
+        // min
+        let yMin = (height + height * kHeightRatio) / 2
+        setupBackgroundLine(x: x, y: yMin, width: lineWidth)
+        setupBackgroundText(x: x, y: yMin, string: min)
+    }
+    
+    private func setupBackgroundLine(x: CGFloat, y: CGFloat, width: CGFloat) {
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: x, y: y))
+        path.addLine(to: CGPoint(x: x + width, y: y))
+        
+        let layer = CAShapeLayer()
+        layer.path = path.cgPath
+        layer.strokeColor = backgroundLineColor.cgColor
+        layer.contentsScale = UIScreen.main.scale
+        
+        self.layer.addSublayer(layer)
+    }
+
+    private func setupBackgroundCenterLine(x: CGFloat, y: CGFloat, width: CGFloat) {
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: x, y: y))
+        path.addLine(to: CGPoint(x: x + width, y: y))
+        
+        let layer = CAShapeLayer()
+        layer.path = path.cgPath
+        layer.strokeColor = backgroundLineColor.cgColor
+        layer.lineWidth = 1
+        layer.lineJoin = kCALineJoinRound
+        layer.lineDashPattern = [NSNumber(value: 2)]
+        layer.contentsScale = UIScreen.main.scale
+        
+        self.layer.addSublayer(layer)
+    }
+    
+    private func setupBackgroundText(x: CGFloat, y: CGFloat, string: String) {
+        let text = CATextLayer()
+        let textWidth: CGFloat = 44.0
+        let textHeight: CGFloat = 12.0
+        let xText = x - textWidth
+        let yText = y - textHeight / 2
+        text.string = string
+        text.foregroundColor = textColor.cgColor
+        text.fontSize = Dimens.Txt.s.rawValue
+        text.font = UIFont.systemFont(ofSize: Dimens.Txt.s.rawValue)
+        text.frame = CGRect(x: xText, y: yText, width: textWidth, height: textHeight)
+        text.contentsScale = UIScreen.main.scale
+        text.alignmentMode = kCAAlignmentLeft
+        
+        self.layer.addSublayer(text)
+    }
+
+    private func drawChart(values: [(close: Double, open: Double, high: Double, low: Double)]) {
+        guard values.count > 0 else { return }
+        
+        let width = bounds.width
+        var x = width * (1 - kWidthRatio) / 2
+
+        for value in values {
+            drawCandlestick(x: x, close: value.close, open: value.open, high: value.high, low: value.low)
+            x += kCandlestickWidth + 1
+        }
+    }
+    
+    private func drawCandlestick(x: CGFloat, close: Double, open: Double, high: Double, low: Double) {
+        let color = open < close ? upColor.cgColor : downColor.cgColor
+
+        // low-high line
+        let xLine = x + kCandlestickWidth / 2
+        let yHigh = getY(high)
+        let linePath = UIBezierPath()
+        linePath.move(to: CGPoint(x: xLine, y: yHigh))
+        linePath.addLine(to: CGPoint(x: xLine, y: getY(low)))
+        cgPoints.append(CGPoint(x: xLine, y: yHigh))
+
+        let lineLayer = CAShapeLayer()
+        lineLayer.path = linePath.cgPath
+        lineLayer.strokeColor = color
+        lineLayer.lineWidth = 1
+        layer.contentsScale = UIScreen.main.scale
+        self.layer.addSublayer(lineLayer)
+
+        // open-close rectangle
+        let y = open < close ? getY(open) : getY(close)
+        var height = open < close ?  getY(open) - getY(close) : getY(close) - getY(open)
+        if height < 0.5 {
+            height = 0.5
+        }
+
+        let rectPath = UIBezierPath(rect: CGRect(x: x, y: y, width: kCandlestickWidth, height: height))
+        
+        let rectLayer = CAShapeLayer()
+        rectLayer.path = rectPath.cgPath
+        rectLayer.fillColor = color
+        layer.contentsScale = UIScreen.main.scale
+
+        self.layer.addSublayer(rectLayer)
+    }
+
+    private func getChangePercentage(value: Double, close: Double, change: Double) -> Double {
+        let prevChange = close + close * change * 0.01
+        return (value - prevChange) / prevChange
+    }
+    
+    /**
+     * @brief base on absMax, seek Y coordinate
+     */
+    private func getY(_ pointY: Double?) -> CGFloat {
+        let height = bounds.height
+        let chartHeight: CGFloat =  kHeightRatio * height
+        let heightMargin: CGFloat = (1 - kHeightRatio) * height / 2
+        guard let pointY = pointY else { return chartHeight + heightMargin }
+        guard max != min else {return chartHeight + heightMargin}
+
+        return CGFloat( (max - pointY) / (max - min) ) * chartHeight + heightMargin
+    }
+
+    private func setupChartAnimation() -> CABasicAnimation {
+        let animation = CABasicAnimation(keyPath: kStrokeEndAnimetionKeyPath)
+        animation.duration = 1
+        animation.fromValue = 0
+        animation.toValue = 1
+        return animation
+    }
+    
+    private func setupTitle() {
+        let width = bounds.width
+        let height = bounds.height
+        let x: CGFloat = (width - width * kWidthRatio) / 2
+        let y: CGFloat = (height - height*kHeightRatio)/4 - Dimens.Txt.l.rawValue/2
+        let textWidth: CGFloat = width*kWidthRatio
+        let textHeight: CGFloat = 20.0
+        
+        setupTitleText(titles.last)
+        titleText.foregroundColor = titleColor.cgColor
+        titleText.fontSize = Dimens.Txt.l.rawValue
+        titleText.font = UIFont.boldSystemFont(ofSize: Dimens.Txt.l.rawValue)
+        titleText.frame = CGRect(x: x, y: y, width: textWidth, height: textHeight)
+        titleText.contentsScale = UIScreen.main.scale
+        titleText.alignmentMode = kCAAlignmentLeft
+        self.layer.addSublayer(titleText)
+    }
+    
+    private func setupTitleText(_ text: String?) {
+        guard let text = text else { return }
+        titleText.string = text
+    }
+
+    private func setupIndicator() {
+        guard let endPoint = cgPoints.last else { return }
+        
+        let x = endPoint.x
+        let y = endPoint.y
+        let absMinY = bounds.height/2 + bounds.height*kHeightRatio/2
+        let margin: CGFloat = 10
+        
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: x, y: y))
+        path.addLine(to: CGPoint(x: x, y: absMinY + margin))
+        
+        indicator.path = path.cgPath
+        indicator.fillColor = UIColor.clear.cgColor
+        indicator.strokeColor = indicatorColor.cgColor
+        indicator.contentsScale = UIScreen.main.scale
+        indicator.lineWidth = 0.5
+        indicator.lineJoin = kCALineJoinRound
+        indicator.lineDashPattern = [NSNumber(value: 2)]
+        indicator.add(setupEndingAnimation(), forKey: kOpacityAnimetionKeyPath)
+        
+        self.layer.addSublayer(indicator)
+    }
+    
+    private func setupIndicatorIcon() {
+        guard let endPoint = cgPoints.last else { return }
+        
+        let path = UIBezierPath(arcCenter: CGPoint(x: endPoint.x, y: endPoint.y), radius: kIconRadius, startAngle: 0.0, endAngle: CGFloat(Double.pi * 2), clockwise: true)
+        indicatorIcon.path = path.cgPath
+        indicatorIcon.fillColor = indicatorColor.cgColor
+        indicatorIcon.contentsScale = UIScreen.main.scale
+        indicatorIcon.opacity = 0.0
+        indicatorIcon.add(setupEndingAnimation(), forKey: kOpacityAnimetionKeyPath)
+        
+        self.layer.addSublayer(indicatorIcon)
+    }
+    
+    private func setupIndicatorText() {
+        guard let endPoint = cgPoints.last else { return }
+        
+        let xText = endPoint.x - kIndicatorTextWidth / 2
+        let absMinY = bounds.height/2 + bounds.height*kHeightRatio/2
+        let margin: CGFloat = 10
+        indicatorText.string = dates.last?.yyyy_mm_dd()
+        indicatorText.alignmentMode = kCAAlignmentCenter
+        indicatorText.foregroundColor = UIColor.white.cgColor
+        indicatorText.backgroundColor = indicatorColor.cgColor
+        indicatorText.fontSize = Dimens.Txt.s.rawValue
+        indicatorText.font = UIFont.boldSystemFont(ofSize: 9)
+        indicatorText.frame = CGRect(x: xText, y: absMinY + margin, width: kIndicatorTextWidth, height: kIndicatorTextHeight)
+        indicatorText.contentsScale = UIScreen.main.scale
+        indicatorText.alignmentMode = kCAAlignmentCenter
+        indicatorText.add(setupEndingAnimation(), forKey: kOpacityAnimetionKeyPath)
+        
+        self.layer.addSublayer(indicatorText)
+    }
+    
+    private func setupEndingAnimation() -> CABasicAnimation {
+        let animation = CABasicAnimation(keyPath: kOpacityAnimetionKeyPath)
+        animation.duration = 1.0
+        animation.beginTime = CACurrentMediaTime() + 0.02
+        animation.fromValue = 0.0
+        animation.toValue = 1.0
+        animation.isRemovedOnCompletion = false
+        animation.fillMode = kCAFillModeBoth
+        return animation
+    }
+    
+    private func moveTo(_ index: Int) {
+        setupTitleText(titles[index])
+        moveIndicatorTo(index)
+        previousIndex = index
+    }
+    
+    private func moveIndicatorTo(_ index: Int) {
+        let cgPoint = cgPoints[index]
+        let xText = cgPoint.x - kIndicatorTextWidth / 2
+        let absMinY = bounds.height / 2 + bounds.height * kHeightRatio / 2
+        let margin: CGFloat = 10
+        
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: cgPoint.x, y: cgPoint.y))
+        path.addLine(to: CGPoint(x: cgPoint.x, y: absMinY + margin))
+        
+        let circlePath = UIBezierPath(arcCenter: CGPoint(x: cgPoint.x, y: cgPoint.y), radius: kIconRadius, startAngle: 0.0, endAngle: CGFloat(Double.pi * 2), clockwise: true)
+        
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        indicatorIcon.path = circlePath.cgPath
+        indicator.path = path.cgPath
+        indicatorText.frame = CGRect(x: xText, y: absMinY + margin, width: kIndicatorTextWidth, height: kIndicatorTextHeight)
+        indicatorText.string = dates[index].yyyy_mm_dd()
+        if #available(iOS 10.0, *), hapticFeedbackEnable {
+            UISelectionFeedbackGenerator().selectionChanged()
+        }
+        CATransaction.commit()
+    }
+
+    private func setupGestureRecognizer() {
+        let gesture = UIPanGestureRecognizer(target: self, action:  #selector(touchEvent))
+        self.addGestureRecognizer(gesture)
+    }
+    
+    @objc func touchEvent(sender: UIPanGestureRecognizer) {
+        guard cgPoints.count > 1 else { return }
+        
+        let x = sender.location(in: self).x
+        let width = bounds.width
+        let k = width * kWidthRatio / CGFloat(cgPoints.count - 1)
+        guard sender.state == .changed, k/2 < abs(x - cgPoints[previousIndex].x) else { return }
+        
+        if x <= cgPoints[0].x + k/2  {
+            moveTo(0)
+        } else if x >= cgPoints[cgPoints.count - 1].x - k/2 {
+            moveTo(cgPoints.count - 1)
+        }
+        
+        let index = AlgorithmSearch.nearestNeighbor(cgPoints.map {$0.x}, searchItem: x)
+        guard previousIndex != index, index > 0, index < cgPoints.count - 1 else { return }
+        
+        moveTo(index)
+    }
+
+}
--- HITChartSwiftSample/HITChartSwiftSample/ViewController.swift
+++ HITChartSwiftSample/HITChartSwiftSample/ViewController.swift
@@ -12,38 +12,38 @@ import HITChartSwift
 class ViewController: UIViewController {
     
     private let data = [
-        (date: "2018/02/28/", close: 10315.00, open: 10583.00, high: 11063.00, low: 10270.00, volume: 43.89, change: -2.40),
-        (date: "2018/03/01/", close: 10925.00, open: 10316.00, high: 11087.00, low: 10224.00, volume: 33.87, change: 5.91),
-        (date: "2018/03/02/", close: 11025.00, open: 10908.00, high: 11189.00, low: 10766.00, volume: 28.86, change: 0.92),
-        (date: "2018/03/03/", close: 11440.00, open: 11024.00, high: 11526.00, low: 11024.00, volume: 30.65, change: 3.76),
-        (date: "2018/03/04/", close: 11501.00, open: 11454.00, high: 11544.00, low: 11061.00, volume: 28.07, change: 0.53),
-        (date: "2018/03/05/", close: 11416.00, open: 11497.00, high: 11696.00, low: 11390.00, volume: 27.56, change: -0.74),
-        (date: "2018/03/06/", close: 10720.00, open: 11403.00, high: 11403.00, low: 10578.00, volume: 44.24, change: -6.10),
-        (date: "2018/03/07/", close: 9902.90, open: 10779.00, high: 10899.00, low: 9422.09, volume: 75.22, change: -7.62),
-        (date: "2018/03/08/", close: 9300.00, open: 9910.70, high: 10109.00, low: 9037.00, volume: 67.75, change: -6.09),
-        (date: "2018/03/09/", close: 9217.00, open: 9301.90, high: 9420.50, low: 8351.00, volume: 98.20, change: -0.89),
-        (date: "2018/03/10/", close: 8762.00, open: 9216.20, high: 9500.00, low: 8691.10, volume: 52.54, change: -4.94),
-        (date: "2018/03/11/", close: 9528.00, open: 8764.40, high: 9726.10, low: 8429.00, volume: 70.48, change: 8.74),
-        (date: "2018/03/12/", close: 9121.00, open: 9528.00, high: 9894.70, low: 8776.80, volume: 67.58, change: -4.27),
-        (date: "2018/03/13/", close: 9135.00, open: 9130.20, high: 9479.00, low: 8827.70, volume: 61.54, change: 0.15),
-        (date: "2018/03/14/", close: 8186.60, open: 9135.20, high: 9400.10, low: 7923.80, volume: 78.81, change: -10.38),
-        (date: "2018/03/15/", close: 8252.90, open: 8181.30, high: 8416.70, low: 7666.30, volume: 82.58, change: 0.81),
-        (date: "2018/03/16/", close: 8251.00, open: 8250.10, high: 8602.70, low: 7903.20, volume: 56.34, change: -0.02),
-        (date: "2018/03/17/", close: 7851.00, open: 8250.00, high: 8350.10, low: 7729.50, volume: 48.97, change: -4.85),
-        (date: "2018/03/18/", close: 8200.20, open: 7832.00, high: 8285.60, low: 7240.00, volume: 88.92, change: 4.45),
-        (date: "2018/03/19/", close: 8600.10, open: 8209.00, high: 8689.90, low: 8100.00, volume: 73.28, change: 4.88),
-        (date: "2018/03/20/", close: 8899.70, open: 8600.20, high: 9025.00, low: 8309.60, volume: 54.28, change: 3.48),
-        (date: "2018/03/21/", close: 8900.10, open: 8899.80, high: 9175.20, low: 8755.70, volume: 42.86, change: 0.00),
-        (date: "2018/03/22/", close: 8706.44, open: 8888.60, high: 9088.00, low: 8453.10, volume: 54.42, change: -2.18),
-        (date: "2018/03/23/", close: 8908.00, open: 8703.80, high: 8910.00, low: 8272.80, volume: 44.91, change: 2.32),
-        (date: "2018/03/24/", close: 8535.00, open: 8911.40, high: 9040.20, low: 8463.00, volume: 44.21, change: -4.19),
-        (date: "2018/03/25/", close: 8445.10, open: 8525.00, high: 8673.90, low: 8373.00, volume: 30.69, change: -1.05),
-        (date: "2018/03/26/", close: 8119.10, open: 8445.00, high: 8496.40, low: 7839.09, volume: 56.38, change: -3.86),
-        (date: "2018/03/27/", close: 7784.50, open: 8128.00, high: 8209.00, low: 7727.00, volume: 50.41, change: -4.12),
-        (date: "2018/03/28/", close: 7870.00, open: 7784.50, high: 8118.00, low: 7725.00, volume: 42.74, change: 1.10)
+        (date: "2018/02/28", close: 10315.00, open: 10583.00, high: 11063.00, low: 10270.00, volume: 43.89, change: -2.40),
+        (date: "2018/03/01", close: 10925.00, open: 10316.00, high: 11087.00, low: 10224.00, volume: 33.87, change: 5.91),
+        (date: "2018/03/02", close: 11025.00, open: 10908.00, high: 11189.00, low: 10766.00, volume: 28.86, change: 0.92),
+        (date: "2018/03/03", close: 11440.00, open: 11024.00, high: 11526.00, low: 11024.00, volume: 30.65, change: 3.76),
+        (date: "2018/03/04", close: 11501.00, open: 11454.00, high: 11544.00, low: 11061.00, volume: 28.07, change: 0.53),
+        (date: "2018/03/05", close: 11416.00, open: 11497.00, high: 11696.00, low: 11390.00, volume: 27.56, change: -0.74),
+        (date: "2018/03/06", close: 10720.00, open: 11403.00, high: 11403.00, low: 10578.00, volume: 44.24, change: -6.10),
+        (date: "2018/03/07", close: 9902.90, open: 10779.00, high: 10899.00, low: 9422.09, volume: 75.22, change: -7.62),
+        (date: "2018/03/08", close: 9300.00, open: 9910.70, high: 10109.00, low: 9037.00, volume: 67.75, change: -6.09),
+        (date: "2018/03/09", close: 9217.00, open: 9301.90, high: 9420.50, low: 8351.00, volume: 98.20, change: -0.89),
+        (date: "2018/03/10", close: 8762.00, open: 9216.20, high: 9500.00, low: 8691.10, volume: 52.54, change: -4.94),
+        (date: "2018/03/11", close: 9528.00, open: 8764.40, high: 9726.10, low: 8429.00, volume: 70.48, change: 8.74),
+        (date: "2018/03/12", close: 9121.00, open: 9528.00, high: 9894.70, low: 8776.80, volume: 67.58, change: -4.27),
+        (date: "2018/03/13", close: 9135.00, open: 9130.20, high: 9479.00, low: 8827.70, volume: 61.54, change: 0.15),
+        (date: "2018/03/14", close: 8186.60, open: 9135.20, high: 9400.10, low: 7923.80, volume: 78.81, change: -10.38),
+        (date: "2018/03/15", close: 8252.90, open: 8181.30, high: 8416.70, low: 7666.30, volume: 82.58, change: 0.81),
+        (date: "2018/03/16", close: 8251.00, open: 8250.10, high: 8602.70, low: 7903.20, volume: 56.34, change: -0.02),
+        (date: "2018/03/17", close: 7851.00, open: 8250.00, high: 8350.10, low: 7729.50, volume: 48.97, change: -4.85),
+        (date: "2018/03/18", close: 8200.20, open: 7832.00, high: 8285.60, low: 7240.00, volume: 88.92, change: 4.45),
+        (date: "2018/03/19", close: 8600.10, open: 8209.00, high: 8689.90, low: 8100.00, volume: 73.28, change: 4.88),
+        (date: "2018/03/20", close: 8899.70, open: 8600.20, high: 9025.00, low: 8309.60, volume: 54.28, change: 3.48),
+        (date: "2018/03/21", close: 8900.10, open: 8899.80, high: 9175.20, low: 8755.70, volume: 42.86, change: 0.00),
+        (date: "2018/03/22", close: 8706.44, open: 8888.60, high: 9088.00, low: 8453.10, volume: 54.42, change: -2.18),
+        (date: "2018/03/23", close: 8908.00, open: 8703.80, high: 8910.00, low: 8272.80, volume: 44.91, change: 2.32),
+        (date: "2018/03/24", close: 8535.00, open: 8911.40, high: 9040.20, low: 8463.00, volume: 44.21, change: -4.19),
+        (date: "2018/03/25", close: 8445.10, open: 8525.00, high: 8673.90, low: 8373.00, volume: 30.69, change: -1.05),
+        (date: "2018/03/26", close: 8119.10, open: 8445.00, high: 8496.40, low: 7839.09, volume: 56.38, change: -3.86),
+        (date: "2018/03/27", close: 7784.50, open: 8128.00, high: 8209.00, low: 7727.00, volume: 50.41, change: -4.12),
+        (date: "2018/03/28", close: 7870.00, open: 7784.50, high: 8118.00, low: 7725.00, volume: 42.74, change: 1.10)
     ]
 
-    private var absMax = 0
+    private var absMaxPercentage = 0
     private var dates = [Date]()
     private var titles = [String]()
     
@@ -54,11 +54,11 @@ class ViewController: UIViewController {
     }
     
     private func formatData() {
-        /// absMax
-        let max = abs(data.map{ $0.change }.max() ?? 0.0).rounded(.up)
-        let min = abs(data.map{ $0.change }.min() ?? 0.0).rounded(.up)
-        absMax = Int(max > min ? max : min)
-        
+        /// absMaxPercentage
+        let maxChange = abs(data.map{ $0.change }.max() ?? 0.0).rounded(.up)
+        let minChange = abs(data.map{ $0.change }.min() ?? 0.0).rounded(.up)
+        absMaxPercentage = Int(maxChange > minChange ? maxChange : minChange)
+
         /// date
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy/MM/dd"
@@ -68,7 +68,7 @@ class ViewController: UIViewController {
         /// title
         titles = data.map { "BTC/USD closing price: \($0.close) change: \($0.change)%  volume: \($0.volume)" }
         
-        tapLineChart(self)
+        tapCandlestickChart(self)
     }
     
     @IBAction func tapLineChart(_ sender: Any) {
@@ -78,7 +78,7 @@ class ViewController: UIViewController {
         view.addSubview(chart)
         let max = String((data.map{ $0.close }.max() ?? 0.0).rounded(.up))
         let min = String((data.map{ $0.close }.min() ?? 0.0).rounded(.down))
-        chart.draw(absMax,
+        chart.draw(absMaxPercentage,
                    values: data.map{ $0.change },
                    label: (max: max, center: "", min: min),
                    dates: dates,
@@ -93,9 +93,24 @@ class ViewController: UIViewController {
         chart.center = view.center
         chart.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi/2))
         view.addSubview(chart)
-        chart.draw(absMax,
+        chart.draw(absMaxPercentage,
                    values: data.map{ $0.change },
-                   label: (max: "+\(absMax)%", center: "0%", min: "-\(absMax)%"),
+                   label: (max: "+\(absMaxPercentage)%", center: "0%", min: "-\(absMaxPercentage)%"),
+                   dates: dates,
+                   titles: titles)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(closeMultiChart(_:)))
+        chart.addGestureRecognizer(tapGesture)
+    }
+    
+    @IBAction func tapCandlestickChart(_ sender: Any) {
+        let chart = HITCandlestickChartView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.height, height: UIScreen.main.bounds.width))
+        chart.center = view.center
+        chart.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi/2))
+        view.addSubview(chart)
+        chart.draw(absMaxPercentage,
+                   values: data.map{ (close: $0.close, open: $0.open, high: $0.high, low: $0.low) },
+                   label: (max: "+\(absMaxPercentage)%", center: "", min: "-\(absMaxPercentage)%"),
                    dates: dates,
                    titles: titles)
         
